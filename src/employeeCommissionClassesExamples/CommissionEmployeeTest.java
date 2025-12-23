a1fdb5b0741bed9efacc830d504b8c056919f533
Cazaks <calebezak@gmail.com>
2025-12-23 23:56:57 +0100

    Test for the Commission employee class

---
+++ src/employeeCommissionClassesExamples/CommissionEmployeeTest.java
@@ -0,0 +1,21 @@
+package employeeCommissionClassesExamples;
+
+public class CommissionEmployeeTest {
+    public static void main(String[] args) {
+
+        CommissionEmployee employee = new CommissionEmployee(
+                "Fred", "Jones", "222222222", 5000.00, .06);
+
+        System.out.println("Employee information obtained by get methods:");
+        System.out.printf("%n%s %s%n", "Frist name is: ", employee.getFirstName());
+        System.out.printf("%s %s%n", employee.getLastName());
+        System.out.printf("%s %s%n", employee.getSocialSecurityNumber());
+        System.out.printf("%s %.2f%n", employee.getCommissionRate());
+        System.out.printf("%s %.2f%n", employee.getCommissionRate());
+
+        employee.setGrossSales(10000.00);
+        employee.setCommissionRate(.1);
+
+        System.out.printf("%n%s:%n%n %n", "Updated employee information obtained by toString", employee );
+    }
+}
