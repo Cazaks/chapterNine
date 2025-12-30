cfe19b02182c1596cea398726264c9c372080089
Cazaks <calebezak@gmail.com>
2025-12-30 03:09:26 +0100

    Modifying the print in Book, PrintBook and the Audio Book classes to look better

--- src/bookPractice/AudioBook.java
+++ src/bookPractice/AudioBook.java
@@ -1,4 +1,33 @@
 package bookPractice;
 
-public class AudioBook {
+public class AudioBook extends Book {
+    private double sizeInMb;
+    private double playLength;
+    private String playBackArtist;
+
+    public AudioBook(String bookTitle, int yearOfPublication, String author,
+                     double sizeInMb,  double playLength, String playBackArtist) {
+        super(bookTitle, yearOfPublication, author);
+        this.sizeInMb = sizeInMb;
+        this.playLength = playLength;
+        this.playBackArtist = playBackArtist;
+    }
+
+    public double getSizeInMb() {
+        return sizeInMb;
+    }
+
+    public double getPlayLength() {
+        return playLength;
+    }
+
+    public String getPlayBackArtist() {
+        return playBackArtist;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%n Size: %.2f MB%n Length %.2f hours %n Narrator: %s",
+                super.toString(),sizeInMb, playLength, playBackArtist);
+    }
 }
--- src/bookPractice/BookTest.java
+++ src/bookPractice/BookTest.java
@@ -1,4 +1,30 @@
 package bookPractice;
 
 public class BookTest {
+    public static void main(String[] args) {
+
+        Book book = new Book("Code practice",
+                2005,
+                "John Smith" );
+
+        PrintBookExt printBook = new PrintBookExt("Arrays for Beginners",
+                2020, "Caleb Cazak", "Agathon ACE",
+                "445 - 123 - 87");
+
+        AudioBook audio = new AudioBook("Read Coding",
+                2024, "Salako Ibrahim",
+                157.8, 2.30, "Ray Lion");
+
+        System.out.println("==========BOOK==========");
+        System.out.println(book);
+
+        System.out.println("\n==========PRINT BOOK==========");
+        System.out.println(printBook);
+
+        System.out.println("\n==========AUDIO BOOK==========");
+        System.out.println(audio);
+
+
+    }
+
 }
